b7f669bd20348fe8628dcd59d59151ed7e9b6b69
J-Meira <jonathanmeira@hotmail.com>
2023-08-31 13:22:29 -0300

    knex config on tests

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,5 +1,14 @@
 import supertest from 'supertest';
 
 import { server } from '../src/server';
+import { Knex } from '../src/database/knex';
+
+beforeAll(async () => {
+  await Knex.migrate.latest();
+});
 
 export const testServer = supertest(server);
+
+afterAll(async () => {
+  await Knex.destroy();
+});
