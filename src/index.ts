2066283314bec17fabdafef14ebcedcdf345d568
J-Meira <jonathanmeira@hotmail.com>
2023-09-01 09:30:08 -0300

    knex migrations and seeds auto run config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
+import { Knex } from './database/knex';
 import { server } from './server';
 
 const port = process.env.PORT || '3000';
 
-server.listen(port, () => console.log(`Running on port: ${port} ...`));
+const startServer = () => {
+  server.listen(port, () => console.log(`Running on port: ${port}`));
+};
+
+if (process.env.IS_DEBUG) {
+  startServer();
+} else {
+  console.log('Started migrations');
+  Knex.migrate
+    .latest()
+    .then(() => {
+      Knex.seed
+        .run()
+        .then(() => startServer())
+        .catch(console.log);
+    })
+    .catch(console.log);
+}
